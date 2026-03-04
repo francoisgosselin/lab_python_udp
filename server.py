13fdb866da20eab3cd0481268c0e2acda29a7744
francoisgosselin <52206358+francoisgosselin@users.noreply.github.com>
2026-03-04 14:31:18 -0500

    Correction server.py

--- server.py
+++ server.py
@@ -71,16 +71,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
     while True:
         data, addr = s.recvfrom(4096)
+        """
+            Pourquoi la valeur de 4096 ici? Éviter les nombres magiques dans le code.
+        """
 
         # Séparer le contenu et le hash
         content, recv_hash = data.split(b"\x00", 1)
 
         # Extraire le nonce (16 octets) et le message
-        nonce = content[:16]
+        nonce = content[:16] # Définir une constante, par exemple NONCE_SIZE = 16
         message = content[16:]
 
         # Recalculer le hash
         calc = hashlib.sha256(nonce + message).digest()
+        """
+        nonce + message = content. Pas besoin d'extraire et de recombiner
+        """
 
         if calc == recv_hash:
             s.sendto(b"Message, nonce et hachage valides", addr)
